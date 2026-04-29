30ca06b8395084d8caa8ce903f607de17d10ed36
shoumen-shomu <shoumenshomu@gmail.com>
2026-04-29 23:55:29 +0600

    Button Components Created

---
+++ src/app/components/Button.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Button = ({className,btnTxt }) => {
+  return (
+    
+    <div className={`px-5.5 py-8.75 text-white inline-block cursor-pointer ${className}`}>{btnTxt}</div>
+    
+  )
+}
+
+export default Button
