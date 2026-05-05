d38681b6a07b1ebc6bff523e6f1d477732cc1e4f
Shoumen Shomu <shoumenshomu@gmail.com>
2026-05-05 18:46:10 +0600

    Container Added

--- src/app/layouts/home/Learn.jsx
+++ src/app/layouts/home/Learn.jsx
@@ -1,8 +1,15 @@
+import Container from '@/app/components/Container'
 import React from 'react'
 
 const Learn = () => {
   return (
-    <div>Learn</div>
+    <>
+    <section className='pt-28.25 pb-36.25 bg-[linear-gradient(90deg,#084FC7_0%,#7F56D9_48%,#7F56D9_100%)]'>
+        <Container>
+            
+        </Container>
+    </section>
+    </>
   )
 }
 
