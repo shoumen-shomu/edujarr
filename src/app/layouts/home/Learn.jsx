e4b9f8e405a19ddcbcd029dc668466fabf8c46ce
shoumen-shomu <shoumenshomu@gmail.com>
2026-05-12 00:22:59 +0600

    Learn Part Done

--- src/app/layouts/home/Learn.jsx
+++ src/app/layouts/home/Learn.jsx
@@ -1,11 +1,15 @@
 import Container from "@/app/components/Container";
 import React from "react";
 import { FaLaptopCode } from "react-icons/fa";
+import { GiGraduateCap } from "react-icons/gi";
+import { BsPersonWorkspace } from "react-icons/bs";
+import Image from "next/image";
 
 const Learn = () => {
   return (
     <>
-      <section className="bg-linear-to-r from-[#084FC7] via-[#7F56D9] via-48% to-[#7F56D9] pt-28.25 pb-36 ">
+      <section className="bg-linear-to-r from-[#084FC7] via-[#7F56D9] via-48% to-[#7F56D9] pt-28.25 pb-36 relative ">
+        <Image src="/images/learnelement.png" alt="" width={180} height={170} className="absolute "/>
         <Container>
           <div className="text-center mb-28.75">
             <h2 className="font-rowdies font-bold text-[45px] text-white leading-12.5 pb-7.25">
@@ -30,7 +34,7 @@ const Learn = () => {
               </p>
             </div>
             <div className="text-center">
-              <FaLaptopCode className="text-white mx-auto" size={80} />
+              <GiGraduateCap  className="text-white mx-auto" size={80} />
               <p className="font-rowdies font-bold text-[25px] text-white leading-8 pt-9 pb-7.25">02. Graduate</p>
               <p className="font-saira font-normal text-xl text-white leading-6.75 w-82.75">
                 Lorem ipsum dolor sit amet, consectetur dolorili adipiscing
@@ -38,7 +42,7 @@ const Learn = () => {
               </p>
             </div>
             <div className="text-center">
-              <FaLaptopCode className="text-white mx-auto" size={80} />
+              <BsPersonWorkspace   className="text-white mx-auto" size={80} />
               <p className="font-rowdies font-bold text-[25px] text-white leading-8 pt-9 pb-7.25">03. Work</p>
               <p className="font-saira font-normal text-xl text-white leading-6.75 w-82.75">
                 Lorem ipsum dolor sit amet, consectetur dolorili adipiscing
