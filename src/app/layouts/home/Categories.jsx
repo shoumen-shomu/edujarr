df6bc6df5a1731d6ef43bea5e66e16ff23ac87f9
shoumen-shomu <shoumenshomu@gmail.com>
2026-05-13 23:59:52 +0600

    heading added

--- src/app/layouts/home/Categories.jsx
+++ src/app/layouts/home/Categories.jsx
@@ -6,7 +6,9 @@ const Categories = () => {
     <>
     <section className='pt-25.25 pb-38.75 bg-linear-to-r from-[#D9ECFF] to-[#D9ECFF]/0'>
     <Container>
-      
+      <div className="">
+        <h2 className='font-rowdies font-bold text-[35px] text-white leading-8.75'>Top Categories</h2>
+      </div>
     </Container>
     </section>
     </>
