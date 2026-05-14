b7fffb2a921649b6e744ade2ce44ad1eb2ddc346
shoumen-shomu <shoumenshomu@gmail.com>
2026-05-14 23:54:15 +0600

    category heading added

--- src/app/layouts/home/Categories.jsx
+++ src/app/layouts/home/Categories.jsx
@@ -6,8 +6,9 @@ const Categories = () => {
     <>
     <section className='pt-25.25 pb-38.75 bg-linear-to-r from-[#D9ECFF] to-[#D9ECFF]/0'>
     <Container>
-      <div className="">
-        <h2 className='font-rowdies font-bold text-[35px] text-white leading-8.75'>Top Categories</h2>
+      <div className="text-center">
+        <h2 className='font-rowdies font-bold text-[35px] text-[#323232] leading-8.75 pb-10.5'>Top <span className=' text-[#2AAA94] '>Categories</span></h2>
+        <p className='font-saira font-medium text-[25px] text-[#4E596B] leading-6.75'>12,000+ unique online course list designs</p>
       </div>
     </Container>
     </section>
