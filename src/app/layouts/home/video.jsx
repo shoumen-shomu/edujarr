873663ed20654585c6f8f18bcc231789b3b47884
shoumen-shomu <shoumenshomu@gmail.com>
2026-05-19 23:35:42 +0600

    design added in image

--- src/app/layouts/home/video.jsx
+++ src/app/layouts/home/video.jsx
@@ -7,7 +7,12 @@ const video = () => {
     <>
     <section>
         <Container>
-            <Image className='mx-auto' src="/images/playimage.png" alt='' width={1373} height={727}/>
+            <div className="relative">
+              <Image className='mx-auto' src="/images/playimage.png" alt='' width={1373} height={727}/>
+              <Image className='absolute bottom-0 right-17' src="/images/design.png" alt='' width={324} height={260}/>
+
+            </div>
+            
         </Container>
     </section>
     </>
