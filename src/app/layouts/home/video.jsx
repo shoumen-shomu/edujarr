5cf2ad88530bb05104a1ff98c401cf8975ea52f0
shoumen-shomu <shoumenshomu@gmail.com>
2026-05-19 23:40:39 +0600

    Play Button added

--- src/app/layouts/home/video.jsx
+++ src/app/layouts/home/video.jsx
@@ -9,7 +9,8 @@ const video = () => {
         <Container>
             <div className="relative">
               <Image className='mx-auto' src="/images/playimage.png" alt='' width={1373} height={727}/>
-              <Image className='absolute bottom-0 right-17' src="/images/design.png" alt='' width={324} height={260}/>
+              <Image className='absolute bottom-0 right-29.5' src="/images/design.png" alt='' width={324} height={260}/>
+              <Image className='absolute top-1/2 left-1/2 -translate-1/2' src="/images/playbtn.png" alt='' width={218} height={218}/>
 
             </div>
             
